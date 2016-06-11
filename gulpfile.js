b4519aef718f9ee4c1012f73bc9127d2b24e070e
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-06-10 20:29:18 -0700

    [config] Get Licence, Readme and package.json up to date

--- gulpfile.js
+++ gulpfile.js
@@ -38,29 +38,29 @@ var imagemin = require('gulp-imagemin');
 var tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build-css', function () {
-    return gulp.src(assetsDev + 'scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([precss, autoprefixer, cssnano]))
-        .pipe(sourcemaps.write())
-        .pipe(ext_replace('.css'))
-        .pipe(gulp.dest(assetsProd + 'css/'));
+  return gulp.src(assetsDev + 'scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([precss, autoprefixer, cssnano]))
+    .pipe(sourcemaps.write())
+    .pipe(ext_replace('.css'))
+    .pipe(gulp.dest(assetsProd + 'css/'));
 });
 
 gulp.task('build-ts', function () {
-    return gulp.src(appDev + '**/*.ts')
-        .pipe(sourcemaps.init())
-        .pipe(typescript(tsProject))
-        .pipe(sourcemaps.write())
-        //.pipe(jsuglify())
-        .pipe(gulp.dest(appProd));
+  return gulp.src(appDev + '**/*.ts')
+    .pipe(sourcemaps.init())
+    .pipe(typescript(tsProject))
+    .pipe(sourcemaps.write())
+    //.pipe(jsuglify())
+    .pipe(gulp.dest(appProd));
 });
 
 gulp.task('build-img', function () {
-    return gulp.src(assetsDev + 'img/**/*')
-        .pipe(imagemin({
-            progressive: true
-        }))
-        .pipe(gulp.dest(assetsProd + 'img/'));
+  return gulp.src(assetsDev + 'img/**/*')
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest(assetsProd + 'img/'));
 });
 
 gulp.task('build-html', function () {
@@ -70,16 +70,14 @@ gulp.task('build-html', function () {
 
 
 gulp.task('move', function(){
-  // the base option sets the relative root for the set of files,
-  // preserving the folder structure
   gulp.src(filesToMove, { base: './' })
   .pipe(gulp.dest(appProd + "/vendor/"));
 });
 
 gulp.task('watch', function () {
-    gulp.watch(appDev + '**/*.ts', ['build-ts']);
-    gulp.watch(assetsDev + 'scss/**/*.scss', ['build-css']);
-    gulp.watch(assetsDev + 'img/*', ['build-img']);
+  gulp.watch(appDev + '**/*.ts', ['build-ts']);
+  gulp.watch(assetsDev + 'scss/**/*.scss', ['build-css']);
+  gulp.watch(assetsDev + 'img/*', ['build-img']);
 });
 
 gulp.task('default', ['watch', 'build-ts', 'build-css']);

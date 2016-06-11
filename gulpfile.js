3ef3e55ca1dbe17400158717a6e56e2cf9a8246a
Catalin Ionescu <catalin.ionescu282@gmail.com>
2016-06-10 21:04:38 -0700

    [config] More gulp hackery to support bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -8,15 +8,18 @@ var appProd = 'app/';
 
 var filesToMove = [
   "node_modules/es6-shim/es6-shim.min.js",
-  "node_modules/systemjs/dist/system-polyfills.js",
+  // "node_modules/systemjs/dist/system-polyfills.js",
   "node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",
   "node_modules/angular2/bundles/angular2-polyfills.js",
   "node_modules/systemjs/dist/system.src.js",
   "node_modules/rxjs/bundles/Rx.js",
   "node_modules/angular2/bundles/angular2.js",
   "node_modules/angular2/bundles/router.dev.js",
-  "node_modules/angular2/bundles/http.js"
+  "node_modules/angular2/bundles/http.js",
 ];
+var cssFilesToMove = [
+  "assets/vendor/bootstrap/**/*"
+]
 
 /* Mixed */
 var ext_replace = require('gulp-ext-replace');
@@ -38,7 +41,7 @@ var imagemin = require('gulp-imagemin');
 var tsProject = typescript.createProject('tsconfig.json');
 
 gulp.task('build-css', function () {
-  return gulp.src(assetsDev + 'scss/*.scss')
+  return gulp.src(assetsDev + '*.scss')
     .pipe(sourcemaps.init())
     .pipe(postcss([precss, autoprefixer, cssnano]))
     .pipe(sourcemaps.write())
@@ -64,14 +67,21 @@ gulp.task('build-img', function () {
 });
 
 gulp.task('build-html', function () {
-    return gulp.src(appDev + '**/*.html')
-        .pipe(gulp.dest(appProd));
+  return gulp.src(appDev + '**/*.html')
+    .pipe(gulp.dest(appProd));
 });
 
 
-gulp.task('move', function(){
+gulp.task('move', ['move-css'], function(cb){
   gulp.src(filesToMove, { base: './' })
-  .pipe(gulp.dest(appProd + "/vendor/"));
+    .pipe(gulp.dest(appProd + "/vendor/"));
+  return cb(null);
+});
+
+gulp.task('move-css', function(cb) {
+  gulp.src(cssFilesToMove)
+    .pipe(gulp.dest(assetsProd + "/vendor/"));
+  return cb(null);
 });
 
 gulp.task('watch', function () {
@@ -80,6 +90,6 @@ gulp.task('watch', function () {
   gulp.watch(assetsDev + 'img/*', ['build-img']);
 });
 
-gulp.task('default', ['watch', 'build-ts', 'build-css']);
+gulp.task('default', ['move', 'build-ts', 'build-css', 'watch']);
 
-gulp.task('build-prod', ['build-ts', 'build-css', 'build-img', 'build-html', 'move'])
+gulp.task('build-prod', ['move', 'build-ts', 'build-css', 'build-img', 'build-html']);

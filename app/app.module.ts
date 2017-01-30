881d80dc60a6b97beff039069e521963fa9a07ac
Catalin Ionescu <catalin.ionescu282@gmail.com>
2017-01-30 17:32:32 +0200

    [routing] Add basic routing to the app component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,41 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { HeroService} from './hero.service';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
+  ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
 })
 export class AppModule { }
